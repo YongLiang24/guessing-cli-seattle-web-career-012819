5e7af9b17a616eceed324cb3206e1fe44db3282a
yong liang <yongliang24@gmail.com>
2019-01-26 17:06:46 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,11 +3,11 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets.chomp
   comp_num = rand(1..6)
-  
+
   if input == "exit"
     puts "Goodbye!"
   end
-  
+
   while input != "exit" do
     if input.to_i == comp_num
       puts "You guessed the correct number!"
