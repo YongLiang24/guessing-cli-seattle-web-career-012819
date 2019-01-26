f0c71a63dd0e4244ca1e70bdbccc88c486f1b558
yong liang <yongliang24@gmail.com>
2019-01-26 17:10:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,14 +1,14 @@
 # Code your solution here!
 def run_guessing_game
-  puts "Guess a number between 1 and 6."
-  input = gets.chomp
-  random_num = rand(1..6)
+  puts "Guess a number between 1 and 6." #Message for the user
+  input = gets.chomp #get input from user
+  random_num = rand(1..6) #generate random numbers 1-6
 
-  if input == "exit"
+  if input == "exit" #print Goodbye when user inputs 'exit'
     puts "Goodbye!"
   end
 
-  while input != "exit" do
+  while input != "exit" do #use while loop to keep the game running as long as input is not 'exit'
     if input.to_i == random_num
       puts "You guessed the correct number!"
     else
